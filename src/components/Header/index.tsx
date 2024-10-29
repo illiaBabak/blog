9c9634f8e4593fadf9aa10363965070fffd1f3ca
dz1to4ka <117407093+illiaBabak@users.noreply.github.com>
2024-10-29 21:21:03 +0100

    Added ability to change theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,8 @@
+import { ThemeBtn } from '../ThemeBtn';
+
 export const Header = (): JSX.Element => (
-  <div className='header w-100 d-flex flex-row align-items-center px-4 py-1'>
-    <h3 className='m-0'>Blog</h3>
+  <div className='header w-100 d-flex flex-row align-items-center justify-content-between px-4 py-1'>
+    <h3 className='m-0 title'>Blog</h3>
+    <ThemeBtn />
   </div>
 );
--- src/root/index.tsx
+++ src/root/index.tsx
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+import { createContext, useEffect, useState } from 'react';
 import { BlogsList } from 'src/components/BlogsList';
 import { Header } from 'src/components/Header';
 import { Database } from 'src/types/supabase';
@@ -6,11 +7,42 @@ import { SUPABASE_URL } from 'src/utils/constants';
 
 export const supabase = createClient<Database>(SUPABASE_URL, import.meta.env.ENV_API_KEY);
 
+type GlobalContextType = {
+  isLightTheme: boolean;
+  setIsLightTheme: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const GlobalContext = createContext<GlobalContextType>({
+  isLightTheme: true,
+  setIsLightTheme: () => {
+    throw new Error('Global context is not initialized');
+  },
+});
+
 export const App = (): JSX.Element => {
+  const [isLightTheme, setIsLightTheme] = useState(() => {
+    const storedData = localStorage.getItem('is_light_theme');
+
+    return storedData ? JSON.parse(storedData) === 'light' : true;
+  });
+
+  useEffect(() => {
+    const { body } = document;
+
+    body.style.setProperty('--primary-color', isLightTheme ? '#ededed' : '#292831');
+    body.style.setProperty('--second-color', isLightTheme ? '#a9a8a8' : '#8f9094');
+    body.style.setProperty('--accent', isLightTheme ? '#5633af' : '#8e55cc');
+    body.style.setProperty('--main', isLightTheme ? '#c8c7c7' : '#333642');
+
+    localStorage.setItem('is_light_theme', JSON.stringify(isLightTheme ? 'light' : 'dark'));
+  }, [isLightTheme]);
+
   return (
-    <div className='d-flex flex-column'>
-      <Header />
-      <BlogsList />
-    </div>
+    <GlobalContext.Provider value={{ isLightTheme, setIsLightTheme }}>
+      <div className='d-flex flex-column'>
+        <Header />
+        <BlogsList />
+      </div>
+    </GlobalContext.Provider>
   );
 };
